563c6a8521bd8735a406da7224b706f7cd1b3804
BEATRIZ GON�ALVES DA SILVA <25171016@educ123.sp.senai.br>
2025-04-29 15:39:24 -0300

    add arquivos hello.py e moldura.py

---
+++ Aula_29_04/hello.py
@@ -0,0 +1,3 @@
+nome = input('Digite seu nome: ')
+
+print(f'\n Seja bem-vindo(a) {nome}')
---
+++ moldura/moldura.py
@@ -0,0 +1,10 @@
+frase = input('\n Digite uma frase: ')
+ch = '\u2588'
+
+for i in range(len(frase)+4):
+    print(ch, end='')
+
+print(f'\n{ch} {frase} {ch}')
+
+for i in range(len(frase)+4):
+    print(ch, end='')
